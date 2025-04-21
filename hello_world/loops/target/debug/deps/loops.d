a7606a876a7a58d4d864b2e2f3521d1b72ee0c4c
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-04-21 15:48:36 +0530

    klklk

---
+++ hello_world/loops/target/debug/deps/loops.d
@@ -0,0 +1,5 @@
+C:\Users\srish\Rust_projects\hello_world\loops\target\debug\deps\loops.exe: src\main.rs
+
+C:\Users\srish\Rust_projects\hello_world\loops\target\debug\deps\loops.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+C:\Users\srish\Rust_projects\hello_world\loops\target\debug\loops.exe: C:\Users\srish\Rust_projects\hello_world\loops\src\main.rs
